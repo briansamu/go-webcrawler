ea328b54901e8e2bf724f60e69009a0420bc29cf
Brian Samu <brian@briansamu.com>
2025-06-15 08:56:12 -0400

    Add .env.example file, update .gitignore to exclude .env, and add MongoDB and dotenv dependencies in go.mod. Refactor main.go to implement database connection and page fetching logic.

--- main.go
+++ main.go
@@ -1,30 +1,100 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
+	"os"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ParseHtml(url string) []byte {
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatalf("Error making GET request: %v", err)
-	}
-	defer resp.Body.Close()
+type DatabaseConnection struct {
+	access     bool
+	uri        string
+	client     *mongo.Client
+	collection *mongo.Collection
+}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading response body: %v", err)
+func (db *DatabaseConnection) connect() {
+	if db.access {
+		db.uri = os.Getenv("MONGO_URI")
+		client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.uri))
+		if err != nil {
+			panic(err)
+		}
+		db.client = client
+		db.collection = db.client.Database("webcrawler").Collection("pages")
+		filter := bson.D{{}}
+		// Deletes all documents in the collection
+		db.collection.DeleteMany(context.TODO(), filter)
 	}
+}
 
-	return body
+func (db *DatabaseConnection) disconnect() {
+	if db.access {
+		db.client.Disconnect(context.TODO())
+		db.access = false
+	}
+}
+
+func (db *DatabaseConnection) insertPage(page Page) {
+	if db.access {
+		db.collection.InsertOne(context.TODO(), page)
+	}
+}
+
+type Page struct {
+	Url     string
+	Title   string
+	Content string
+}
+
+func fetchPage(url string) (string, string, error) {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	var title, content string
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(`body`, chromedp.ByQuery),
+		chromedp.Sleep(1500*time.Millisecond), // Wait for hydration
+		chromedp.Title(&title),
+		chromedp.OuterHTML(`html`, &content, chromedp.ByQuery),
+	)
+
+	return title, content, err
 }
 
 func main() {
-	pageBody := ParseHtml("https://samuinteractive.com")
-	fmt.Println(string(pageBody))
+	dbAccess := true
+
+	if godotenv.Load() != nil {
+		fmt.Println("Error loading .env file")
+		dbAccess = false
+	}
+
+	db := DatabaseConnection{access: dbAccess, uri: "", client: nil, collection: nil}
+	db.connect()
+
+	seed := os.Getenv("SEED_URL")
+	title, content, err := fetchPage(seed)
+	if err != nil {
+		fmt.Println("Error fetching page:", err)
+		return
+	}
+
+	db.insertPage(Page{Url: seed, Title: title, Content: content})
+
+	db.disconnect()
 }
 
 // Add page to queue
